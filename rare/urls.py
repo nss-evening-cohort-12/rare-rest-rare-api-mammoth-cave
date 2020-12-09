640610ac3ef1cc5c8816ba14d00f6aca5557258c
Geno McNew <genemcnew3@gmail.com>
2020-12-08 19:06:56 -0600

    added auth. Can register new user. Can log in. Added fixtures for a user

--- rare/urls.py
+++ rare/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rareapi.views import register_user, login_user
 
 urlpatterns = [
+    
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
 ]
