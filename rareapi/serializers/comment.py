662c1b4fba1a935cd42d379391d9d7aefa884550
NateOwens <nathanieltylerowens@gmail.com>
2020-12-17 19:54:47 -0600

    wip - checking with frontend

--- rareapi/serializers/comment.py
+++ rareapi/serializers/comment.py
@@ -1,13 +1,36 @@
 from rest_framework import serializers
-from rareapi.models import Comment, Post
+from rareapi.models import Comment, Post, RareUser 
+from django.contrib.auth.models import User
 
 class CommentSerializer(serializers.ModelSerializer):
   class Meta:
     model = Comment
     fields = ('id', 'author_id', 'post_id', 'content', 'subject', 'created_on')
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name']
+
+class RareUserSerializer(serializers.ModelSerializer):
+    user_id = UserSerializer(read_only=True)
+    class Meta:
+        model = RareUser
+        fields = ('id', 'user_id')
+        depth = 1
+
 class PostSerializer(serializers.ModelSerializer):
   class Meta:
     model = Post
     fields = ('id', 'user_id', 'category_id', 'title', 'publication_date', 'image_url', 'content', 'approved', 'tags')
     depth = 1
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Comment
+    fields = ('id', 'author_id', 'post_id', 'content', 'subject', 'created_on')
+
+class CommentUpdateSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Comment
+    fields = ('id', 'author_id', 'post_id', 'content', 'subject', 'created_on')
--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -1,7 +1,35 @@
+from django.http.response import HttpResponseBadRequest
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rareapi.models import Comment
-from rareapi.serializers import CommentSerializer
+from rareapi.serializers import CommentSerializer, CommentCreateSerializer, CommentUpdateSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
-  queryset = Comment.objects.all()
-  serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def create(self, request):
+      serializer = CommentCreateSerializer(data=request.data)
+      if serializer.is_valid(raise_exception=True):
+          serializer.save()
+          return Response(serializer.data)
+      else:
+          return Response(serializer.errors, status=HttpResponseBadRequest.status_code)
+    
+    
+    def update(self, request, pk):
+      post = self.get_object()
+      serializer = CommentUpdateSerializer(post, data=request.data, partial=False)
+      if serializer.is_valid(raise_exception=True):
+          serializer.save()
+          return Response(serializer.data)
+      else:
+          return Response(serializer.errors, status=HttpResponseBadRequest.status_code)
+
+    def get_queryset(self):
+      post_id = self.request.query_params.get('post_id', None)
+      if post_id:
+        return self.queryset.filter(post_id=post_id)
+      else:
+        return self.queryset
+
