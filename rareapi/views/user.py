16221e98adb28dcf636488718f56c0b578e259e1
NateOwens <nathanieltylerowens@gmail.com>
2021-01-12 18:09:41 -0600

    added before merge

--- rareapi/views/user.py
+++ rareapi/views/user.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
 from rareapi.models import RareUser
 from rareapi.serializers import RareUserSerializer, DetailedRareUserSerializer
+from rareapi.serializers.rare_user import RareUserUpdateSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = RareUser.objects.all()
+
+    def update(self, request, pk):
+      post = self.get_object()
+      serializer = RareUserUpdateSerializer(post, data=request.data, partial=False)
+      if serializer.is_valid(raise_exception=True):
+          serializer.save()
+          return Response(serializer.data)
+      else:
+          return Response(serializer.errors, status=HttpResponseBadRequest.status_code)
 
 
     def get_serializer_class(self):
